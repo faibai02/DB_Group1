126459320fb46676602f4e5840e58a6a0cfbcdad
faibai02 <janis.reisenauer@googlemail.com>
2026-01-12 17:26:53 +0900

    Start

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="page about-page">
+            <h1>About Us</h1>
+            <p>We are building a great organized React app.</p>
+        </div>
+    );
+};
+
+export default About;
